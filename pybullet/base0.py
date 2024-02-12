e8f6ad35dee8d1ac99c2fbcfb6aa4aa26b89e497
jfisher2021 <j.fisher.2021@alumnos.urjc.es>
2024-02-12 21:32:27 +0100

    probando

--- pybullet/base0.py
+++ pybullet/base0.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 
-urdf_path = "urdf/example_continuous.urdf"
+urdf_path = "urdf/practica0.urdf"
 
 physicsClient = p.connect(p.GUI) #or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
@@ -20,20 +20,12 @@ print("NumJoints: {}".format(numJoints))
 for j in range(numJoints):
      print("{} - {}".format(p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
 
-
-right_id = p.addUserDebugParameter("rightMotor", -1, 1, 0)
-left_id = p.addUserDebugParameter("leftMotor", -1, 1, 0)
-
 try:
     while True:
         p.stepSimulation()
         time.sleep(1./240.)
 
-        right_motor_value = p.readUserDebugParameter(right_id)
-        left_motor_value = p.readUserDebugParameter(left_id)
         
-        p.setJointMotorControl2(robotId,1, p.VELOCITY_CONTROL, targetVelocity=right_motor_value)
-        p.setJointMotorControl2(robotId,0, p.VELOCITY_CONTROL, targetVelocity=left_motor_value)
         
 except KeyboardInterrupt:
       pass
