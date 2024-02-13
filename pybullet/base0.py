b47e37124eb226669513be19dc5000d29ef35594
Jonathan Fisher <j.fisher.2021@alumnos.urjc.es>
2024-02-13 10:17:53 +0100

    probando2

--- pybullet/base0.py
+++ pybullet/base0.py
@@ -15,18 +15,28 @@ startOrientation = p.getQuaternionFromEuler([0,0,-3.15])
 
 robotId = p.loadURDF(urdf_path,startPos, startOrientation)
 
+frictionId = p.addUserDebugParameter("DOOR_jointFriction", 0, 100, 10)
+torqueId = p.addUserDebugParameter("DOOR_joint torque", -20, 20, -9)
 numJoints = p.getNumJoints(robotId)
 print("NumJoints: {}".format(numJoints))
 for j in range(numJoints):
      print("{} - {}".format(p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
 
+speedId = p.addUserDebugParameter("R2D2_speed", 0, 40, 5)
+torqueId = p.addUserDebugParameter("R2D2_force", 0, 40, 5)
 try:
     while True:
         p.stepSimulation()
         time.sleep(1./240.)
+        speed = p.readUserDebugParameter(speedId)
+        torque = p.readUserDebugParameter(torqueId)
 
-        
-        
+                
+        frictionForce = p.readUserDebugParameter(frictionId)
+        jointTorque = p.readUserDebugParameter(torqueId)
+        #set the joint friction
+        p.setJointMotorControl2(robotId, 1, p.VELOCITY_CONTROL, targetVelocity=0, force=frictionForce)
+        #apply a joint torque
 except KeyboardInterrupt:
       pass
 	
