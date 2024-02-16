029d4b4bcabf0198fbf1f9b259886e7502326f3b
Jonathan Fisher <j.fisher.2021@alumnos.urjc.es>
2024-02-16 19:38:27 +0100

    b

--- pybullet/escenario.py
+++ pybullet/escenario.py
@@ -2,13 +2,15 @@ import pybullet as p
 import time
 import pybullet_data
 
-urdf_path = "urdf/practica0.urdf"
+urdf_path = "urdf/escenario.urdf"
 
 physicsClient = p.connect(p.GUI) #or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane_transparent.urdf")
+robotId = p.loadURDF("husky/husky.urdf")
+
 
 startPos = [0,0,1]
 startOrientation = p.getQuaternionFromEuler([0,0,-3.15])
@@ -20,19 +22,13 @@ print("NumJoints: {}".format(numJoints))
 for j in range(numJoints):
      print("{} - {}".format(p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
 
-frictionId = p.addUserDebugParameter("jointFriction", 0, 20, 10)
-torqueId = p.addUserDebugParameter("torque force", 0, 40, 0)
-velocityId = p.addUserDebugParameter("jointVelocity", -20, 20, 0)
+
 try:
     while True:
         
-        jointTorque = p.readUserDebugParameter(torqueId)
-        frictionForce = p.readUserDebugParameter(frictionId)
-        velocity = p.readUserDebugParameter(velocityId)
+
           
-        p.setJointMotorControl2(robotId, 1, p.VELOCITY_CONTROL, targetVelocity=0, force=frictionForce)
-        p.setJointMotorControl2(robotId, 1, p.TORQUE_CONTROL, force=jointTorque)
-        p.setJointMotorControl2(robotId, 1, p.VELOCITY_CONTROL, targetVelocity=velocity, force=frictionForce)
+        
         p.stepSimulation()
         time.sleep(1./240.)
 
