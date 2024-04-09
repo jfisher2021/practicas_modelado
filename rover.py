92cf16d571dc524d26820813d90dce071165a1b6
Jonathan Fisher <j.fisher.2021@alumnos.urjc.es>
2024-04-09 18:32:36 +0200

    Me va en 20 seg

--- rover.py
+++ rover.py
@@ -2,7 +2,6 @@ import pybullet as p
 import time
 import pybullet_data
 
-
 car = "urdf/probando.urdf"
 caja = "urdf/caja.urdf"
 
@@ -14,21 +13,32 @@ p.setGravity(0,0,-9.8)
 # Load the plane
 planeId = p.loadURDF("plane.urdf")
 
-# Define initial positions and orientations
-startPos = [0,0,3]
-cajaPos = [0,3,0]
-pos_dejar = [0,-0.5,1.5]
+#### STATE MACHINE ####
+ACERCRSE = 1
+ARRIBA_CAJA = 2
+COGER_CAJA = 3
+CLOSE_GRIPPER = 4
+ARRIBA_ROBOT = 5
+DEJAR_CAJA = 6
+ACTUAL = ACERCRSE
+
+########### POSITIONS ############
+startPos = [0,0,2]
+UpBox = [0,4,2]
+boxPos = [0,3.9,0]
+UpCar = [0,4,3.5]
+InCar = [0,2,3]
 
 # Constants for controlling the robot
-velocity = 11
-force = 25
+velocity = 20
+force = 20
 
 current_time = time.time()
 # Load URDF models
 carOrientation = p.getQuaternionFromEuler([0,0,3.15])
 
 carModel = p.loadURDF(car, startPos, carOrientation)
-cajaModel = p.loadURDF(caja, cajaPos, carOrientation)
+cajaModel = p.loadURDF(caja, boxPos, carOrientation)
 
 
 # Get the number of joints in the ramp model
@@ -36,52 +46,130 @@ numJoints = p.getNumJoints(carModel)
 print("NumJoints: {}".format(numJoints))
 for j in range(numJoints):
       print("{} - {}".format(p.getJointInfo(carModel,j)[0], p.getJointInfo(carModel,j)[1].decode("utf-8")))
-
+armJoints = [0, 1, 3]
+endEfector = [5, 6]
 # Enable real-time simulation
 p.setRealTimeSimulation(1)
 
-
+prevPose = [0, 0, 0]
+prevPose1 = [0, 0, 0]
+hasPrevPose = 1
 
 # Solo queremos que la cinemática inversa actue sobre los primeros 3 JOINTS
-robotEndEffectorIndex=3
+robotEndEffectorIndex=4
+going = True
+
+# Vamos a posicionar el brazo por una diagonal X,Y usando la misma altura (Z)
+# El rango máximo/minimo definido
+range_m=[0.3,2.0]
+
+c=range_m[0]
+
+trailDuration = 5
+
+# Solo queremos que la inemática inversa actue sobre los primeros 5 JOINTS
+robotEndEffectorIndex=5
 
 try:
-      
+      current_pos = -1
       # part of 3.2
       lateralFriction = 0.93
-      spinningFriction = 0.005
-      rollingFriction = 0.003
+      spinningFriction = 1
       for i in range(2, 6):
-            p.changeDynamics(carModel, i, lateralFriction=lateralFriction, spinningFriction=spinningFriction, rollingFriction=rollingFriction)
+            p.changeDynamics(carModel, i, lateralFriction=lateralFriction, spinningFriction=spinningFriction)
 
-    
-      
       while True:
+           
                   
-            jointPoses = p.calculateInverseKinematics(carModel, robotEndEffectorIndex, cajaPos  )
-            print(jointPoses)
-            p.addUserDebugText("X", cajaPos  , [1,0,0], 1)
+            # Get the position and orientation of the car
+            carPos, carOri = p.getBasePositionAndOrientation(carModel)
+            pos_y = carPos[1]
+            
+            if (ACTUAL == ACERCRSE):
+                  # Calcular la distancia restante hasta el punto de detención (1 metro antes de la caja)
+                  distancia_restante = 1 - pos_y
+                  # Ajustar la velocidad proporcionalmente a la distancia restante, con un mínimo de 0
+                  actual_force = force * (distancia_restante)
+                  # Aplicar la velocidad actual al robot
+                  p.setJointMotorControlArray(carModel, [7, 8, 9, 10], p.VELOCITY_CONTROL, targetVelocities=[velocity] * 4, forces=[actual_force] * 4)
+                  
+                  if pos_y >= 0.8:
+                        ACTUAL = ARRIBA_CAJA      
+                        print("ARRIBA_CAJA")
+                        
+            elif (ACTUAL == ARRIBA_CAJA):
+                  p.setJointMotorControlArray(carModel, [7,8,9,10], p.VELOCITY_CONTROL, targetVelocities=[0] * 4, forces=[100] * 4)
+                  
+                  jointPoses = p.calculateInverseKinematics(carModel, robotEndEffectorIndex, UpBox)
+                  p.addUserDebugText("X", UpBox  , [1,0,0], 1)
+            
+                  p.setJointMotorControlArray(carModel, armJoints, p.POSITION_CONTROL,targetPositions=jointPoses[0:len(armJoints)],forces=[2000]*len(armJoints),positionGains=[0.01]*len(armJoints),velocityGains=[2]*len(armJoints))
+                  # sacar posicion del brazo
+                  ls = p.getLinkState(carModel, robotEndEffectorIndex)
+                  position = ls[0] 
+                  print("ls1: {}".format(position[1]))
+                  print("ls2: {}".format(position[2]))
+                  if (position[1] >= 4 and position[2] >= 1.99):
+                        ACTUAL = COGER_CAJA
+                        print("COGER_CAJA")
+                  
+            elif (ACTUAL == COGER_CAJA):
+                  jointPoses = p.calculateInverseKinematics(carModel, robotEndEffectorIndex, boxPos)
+                  p.addUserDebugText("X", boxPos  , [1,0,0], 1)
+            
+                  p.setJointMotorControlArray(carModel, armJoints, p.POSITION_CONTROL,targetPositions=jointPoses[0:len(armJoints)],forces=[2000]*len(armJoints),positionGains=[0.01]*len(armJoints),velocityGains=[2]*len(armJoints))
+                  # sacar posicion del brazo
+                  ls = p.getLinkState(carModel, robotEndEffectorIndex)
+                  
+                  
+                  position = ls[0] 
+                  
+                  print("ls1: {}".format(position[1]))
+                  print("ls2: {}".format(position[2]))
+                  if (position[1] <= 4.1 and position[2] <= 0.38):
+                        ACTUAL = CLOSE_GRIPPER
+                        print("CLOSE_GRIPPER")
+            elif (ACTUAL == CLOSE_GRIPPER):
+                  p.setJointMotorControlArray(carModel, [5,6], p.POSITION_CONTROL, targetPositions=[-0.20,-0.20],forces=[300]*2)
+                  time.sleep(1)
+                  ACTUAL = ARRIBA_ROBOT
+                  print("ARRIBA_ROBOT")
+            elif (ACTUAL == ARRIBA_ROBOT):
+                  jointPoses = p.calculateInverseKinematics(carModel, robotEndEffectorIndex, UpCar)
+                  p.addUserDebugText("X", UpCar  , [1,0,0], 1)
+            
+                  p.setJointMotorControlArray(carModel, armJoints, p.POSITION_CONTROL,targetPositions=jointPoses[0:len(armJoints)],forces=[2000]*len(armJoints),positionGains=[0.01]*len(armJoints),velocityGains=[2]*len(armJoints))
+                  # sacar posicion del brazo
+                  ls = p.getLinkState(carModel, robotEndEffectorIndex)
+                  
+                  
+                  position = ls[0] 
+                  
+                  print("ls1: {}".format(position[1]))
+                  print("ls2: {}".format(position[2]))
+                  if (position[2] >= 3.4):
+                        ACTUAL = DEJAR_CAJA
+                        print("DEJAR_CAJA")
+            elif (ACTUAL == DEJAR_CAJA):
+                  jointPoses = p.calculateInverseKinematics(carModel, robotEndEffectorIndex, InCar)
+                  p.addUserDebugText("X", InCar  , [1,0,0], 1)
+            
+                  p.setJointMotorControlArray(carModel, armJoints, p.POSITION_CONTROL,targetPositions=jointPoses[0:len(armJoints)],forces=[2000]*len(armJoints),positionGains=[0.01]*len(armJoints),velocityGains=[2]*len(armJoints))
+                  # sacar posicion del brazo
+                  ls = p.getLinkState(carModel, robotEndEffectorIndex)
+                  
+                  
+                  position = ls[0] 
+                  
+                  print("ls1: {}".format(position[1]))
+                  print("ls2: {}".format(position[2]))
+                  if (position[1] <= 0.33 and position[2] <= 3):
+                        p.setJointMotorControlArray(carModel, [5,6], p.POSITION_CONTROL, targetPositions=[0.0,0.0],forces=[10]*2)
 
-
-            for i in range(len(jointPoses)):
-                  p.setJointMotorControl2(bodyIndex=carModel,
-                                                      jointIndex=i,
-                                                      controlMode=p.POSITION_CONTROL,
-                                                      targetPosition=jointPoses[i],
-                                                      targetVelocity=0,
-                                                      force=500,
-                                                      positionGain=0.03,
-                                                      velocityGain=1)
-            # Set the new camera target position
-            #p.resetDebugVisualizerCamera(cam_distance, cam_yaw, cam_pitch, cam_target_pos)
-
-    
-            # p.setJointMotorControlArray(carModel, [2, 3, 4, 5], p.VELOCITY_CONTROL, targetVelocities=[velocity] * 4, forces=[force] * 4)
-            # # Get wheel velocities
-            # wheel_velocities = [p.getJointState(carModel, i)[1] for i in [2, 3, 4, 5]]
-            # # Get wheel forces
-            # wheel_forces = [p.getJointState(carModel, i)[3] for i in [2, 3, 4, 5]]
-            # Add the data to the list
+                  
+            
+                  
+                   
 
 except KeyboardInterrupt:
    pass
